867dca6ff7f2171bb600b36991560726daef0106
Minh Phàm Dân <phamdanminh@gmail.com>
2025-07-10 21:15:29 +0700

    modified config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8080/api/v1';
-export const WEB_URL = import.meta.env.WEB_URL || 'http://192.168.0.155:3002';
+export const WEB_URL = import.meta.env.WEB_URL || 'http://127.0.0.1:3000';
